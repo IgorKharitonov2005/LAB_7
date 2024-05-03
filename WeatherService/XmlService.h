32d3cb4725d5200a0f22f53c6886aace41a8e128
Igor Kharitonov <stud179787@vyatsu.ru>
2024-05-03 10:39:44 +0300

    Created XML

---
+++ WeatherService/XmlService.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Service.h"
+#include "Weather.h"
+class XmlService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~XmlService() {};
+};
